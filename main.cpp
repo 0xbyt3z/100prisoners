27791cf69d1c513f9a6d800d3e19a5ced863cb37
theekshana <rtheekshanafernando@gmail.com>
2022-08-21 12:25:31 +0530

    final

--- main.cpp
+++ main.cpp
@@ -13,8 +13,13 @@ https://www.youtube.com/watch?v=iSNsgj1OCLA&t=629s
 
 */
 
+void foundMessage(int, int);
+void log(int, int, int);
+
 int main()
 {
+    //results
+    int successcount;
 
     // file stream to read generated values from the python program
     fstream file;
@@ -37,6 +42,68 @@ int main()
 
     // start cycles
     // consider the prisnored are queued according to their respective number
+    for (int i = 0; i < 100; i++)
+    {
+        // each prisoner will get 50 chances
+        int attempts = 1;
+        // current reference ( start from the prisoner number)
+        int currentref;
+        // each prisoner start looking from their own number
+        currentref = room.at(i);
+        int next;
+        bool found = false;
+        // log(i, currentref, attempts);
+        //cout << i << " :  " << room.at(i) << "\n";
+        if (currentref == i)
+        {
+            // found in the first attempt
+            found = true;
+            foundMessage(i, 1);
+        }
+        while (!found)
+        {
+            // log(i, currentref, attempts);
+            if (attempts < 50)
+            {
+                //cout << i << " :  " << room.at(currentref) << "\n";
+                next = room.at(currentref);
+                if (next == i)
+                {
+                    found = true;
+                    foundMessage(i, attempts);
+                    successcount++;
+                    break;
+                }
+                currentref = next;
+            }
+            else
+            {
+                foundMessage(i, 51);
+                break;
+            }
+            attempts++;
+        }
+    }
 
+    cout << "\n\n-----------------Results-----------------\n" <<
+        "Number of Prisoners           : " << 100 <<
+        "\nSuccess Rate                  : " << successcount << " %" <<
+        "\n-----------------------------------------";
     return 0;
+}
+
+void foundMessage(int pid, int attempts)
+{
+    if (attempts > 50)
+    {
+        cout << "- Prisoner " << pid << " did not found within 50 attempts\n";
+    }
+    else {
+        cout << "+ Prisoner " << pid << " found in " << attempts << " attempts\n";
+    }
+}
+
+void log(int pid, int value, int attempts)
+{
+    cout << "+ Prisoner : " << pid << " Opened : " << value << " Attempts : " << attempts << "\n";
 }
