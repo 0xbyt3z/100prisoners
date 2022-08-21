785f5b4e9de636a8c0fc25be8032545fd5d25779
Theekshana Fernando <40062006+0xbyt3z@users.noreply.github.com>
2022-08-21 12:34:22 +0530

    add comments

--- shuffleList.py
+++ shuffleList.py
@@ -1,14 +1,21 @@
 import random
 
+# create a sorted list from 0 -99
 l = [x for x in range(0, 100)]
+
+#  reversing the list may increase the randomness
 l.reverse()
+
+#shuffles the list
 random.shuffle(l)
 
+# file for the c++ program
 with open("list.txt", "w") as file:
     for i in l:
         s = f"{i}\n"
         file.write(s)
 
+# file to map prisoners and boxes to 0th index
 with open("ref.txt", "w") as file:
     counter = 0
     for i in l:
