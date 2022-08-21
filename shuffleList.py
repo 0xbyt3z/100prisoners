6d3e8fc1d2ab7e41518ff3108fde2d8d3ee1e481
theekshana <rtheekshanafernando@gmail.com>
2022-08-21 12:24:15 +0530

    add a ref.txt to map prisoners and boxes to 0th index

--- shuffleList.py
+++ shuffleList.py
@@ -1,10 +1,18 @@
 import random
 
-l = [x for x in range(1, 101)]
-
+l = [x for x in range(0, 100)]
+l.reverse()
 random.shuffle(l)
 
 with open("list.txt", "w") as file:
     for i in l:
         s = f"{i}\n"
+        file.write(s)
+
+with open("ref.txt", "w") as file:
+    counter = 0
+    for i in l:
+        # prisoner : box
+        s = f"{counter} : {i}\n"
+        counter += 1
         file.write(s)
